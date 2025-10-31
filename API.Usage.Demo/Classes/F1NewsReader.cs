53cac397aadb344202e3c8e45c13e2064b8aace0
Baxter Pearson <baxterrpearson@gmail.com>
2025-10-31 14:01:48 +0000

    Add project files.

---
+++ API.Usage.Demo/Classes/F1NewsReader.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace API.Usage.Demo.Classes
+{
+    public class F1NewsItem
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public DateTime PubDate { get; set; }
+        public string ImageURL { get; set; }
+    }
+
+    public class F1NewsResponse
+    {
+        public List<F1NewsItem> F1News { get; set; }
+    }
+
+    public class F1NewsReader
+    {
+        public async static Task<F1NewsResponse?> GetF1News()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://f1-news1.p.rapidapi.com/f1news/0/0"),
+                Headers =
+                {
+                    { "x-rapidapi-key", "03b95f15c1msh25adab3237d4b36p107fc2jsn8ca895c5ea85" },
+                    { "x-rapidapi-host", "f1-news1.p.rapidapi.com" },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var result  = Parse(body);
+
+                return result;
+            }
+        }
+
+        public static F1NewsResponse Parse(string json)
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            return JsonSerializer.Deserialize<F1NewsResponse>(json, options);
+        }
+    }
+}
---
+++ API.Usage.Demo/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using System.Diagnostics;
+using API.Usage.Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+using API.Usage.Demo.Classes;
+
+namespace API.Usage.Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> F1News()
+        {
+            var result = await F1NewsReader.GetF1News();
+            return View(result);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
