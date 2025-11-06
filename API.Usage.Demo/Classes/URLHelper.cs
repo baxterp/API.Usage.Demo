6fe41b3c7c4c8b93c10ea86d04bfc24a5d73d5b1
Baxter Pearson <baxterrpearson@gmail.com>
2025-11-06 14:51:18 +0000

    Fixed image URL issues, and resized stock image for each slide

---
+++ API.Usage.Demo/Classes/URLHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Usage.Demo.Classes
+{
+    public static class URLHelper
+    {
+        public static bool IsValidUrl(string url)
+        {
+            return Uri.TryCreate(url, UriKind.Absolute, out Uri? uriResult)
+                   && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+        }
+    }
+}
